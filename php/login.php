3d1a75601525e663af08c0818a68c850da3acbc2
Gabriel Gomez <132381579+Gopi1794@users.noreply.github.com>
2024-08-17 11:37:29 -0300

    Actualizar login.php
    Le faltaba el punto y coma en el segundo include

--- php/login.php
+++ php/login.php
@@ -1,6 +1,6 @@
   <?php
                     include("conexion.php");
-                    include("registro_usuarios_be.php")
+                    include("registro_usuarios_be.php");
                     ?>
 <!DOCTYPE html>
 <html lang="es">
