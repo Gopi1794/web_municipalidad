6578eca90dee68a6061f91ade63e5e1ade80f79d
Gabriel Gomez <132381579+Gopi1794@users.noreply.github.com>
2025-02-19 20:38:30 -0300

    favicon y google analitics añadido

--- public/misanvicente/ciudadano/login_ciudadanos.php
+++ public/misanvicente/ciudadano/login_ciudadanos.php
@@ -4,10 +4,51 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ingreso - Mi San Vicente</title>
+
+    <meta property="og:title" content="Mi San Vicente">
+    <meta property="og:description" content="Gestion de persona de la Municipalidad de San Vicente.">
+    <meta property="og:image" content="https://webmunicipal.online/public/img/logos_sanvicente/logo_misanvicente.png">
+    <meta property="og:url" content="https://webmunicipal.online/public/misanvicente/ciudadano/index.php">
+    <meta property="og:type" content="website">
+
+    <meta name="description" content="Sistema de gestión de personas de la Municipalidad de San Vicente. Accede a la administración y gestión de trámites.">
+    <meta name="keywords" content="San Vicente, Municipalidad, gestión, trámites, gobierno">
+    <meta name="author" content="Municipalidad de San Vicente">
+    <meta name="robots" content="index, follow">
+
+    <link rel="icon" type="image/png" sizes="16x16" href="../../../favicon/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../../../favicon/favicon-32x32.png">
+
+    <!-- Apple Touch Icon (para iOS) -->
+    <link rel="apple-touch-icon" sizes="180x180" href="../../../favicon/apple-touch-icon.png">
+
+    <!-- Android Chrome Icons -->
+    <link rel="icon" type="image/png" sizes="192x192" href="../../../favicon/android-chrome-192x192.png">
+    <link rel="icon" type="image/png" sizes="512x512" href="../../../favicon/android-chrome-512x512.png">
+
+
+    <!-- Twitter Card (para Twitter) -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Mi San Vicente">
+    <meta name="twitter:description" content="Gestion de persona de la Municipalidad de San Vicente.">
+    <meta name="twitter:image" content="https://webmunicipal.online/public/img/logos_sanvicente/logo_misanvicente.png">
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-TE38TW45ZY"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-TE38TW45ZY');
+    </script>
     <link rel="stylesheet" href="../../css/style-register.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <title>Ingreso - Mi San Vicente</title>
 </head>
 
 <body>
--- public/misanvicente/ciudadano/revalidacion_email.php
+++ public/misanvicente/ciudadano/revalidacion_email.php
@@ -5,6 +5,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mi San Vicente</title>
+    <meta property="og:title" content="Mi San Vicente">
+    <meta property="og:description" content="Gestion de persona de la Municipalidad de San Vicente.">
+    <meta property="og:image" content="https://webmunicipal.online/public/img/logos_sanvicente/logo_misanvicente.png">
+    <meta property="og:url" content="https://webmunicipal.online/public/misanvicente/ciudadano/index.php">
+    <meta property="og:type" content="website">
+
+    <meta name="description" content="Sistema de gestión de personas de la Municipalidad de San Vicente. Accede a la administración y gestión de trámites.">
+    <meta name="keywords" content="San Vicente, Municipalidad, gestión, trámites, gobierno">
+    <meta name="author" content="Municipalidad de San Vicente">
+    <meta name="robots" content="index, follow">
+
+    <link rel="icon" type="image/png" sizes="16x16" href="../../../favicon/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../../../favicon/favicon-32x32.png">
+
+    <!-- Apple Touch Icon (para iOS) -->
+    <link rel="apple-touch-icon" sizes="180x180" href="../../../favicon/apple-touch-icon.png">
+
+    <!-- Android Chrome Icons -->
+    <link rel="icon" type="image/png" sizes="192x192" href="../../../favicon/android-chrome-192x192.png">
+    <link rel="icon" type="image/png" sizes="512x512" href="../../../favicon/android-chrome-512x512.png">
+
+
+    <!-- Twitter Card (para Twitter) -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Mi San Vicente">
+    <meta name="twitter:description" content="Gestion de persona de la Municipalidad de San Vicente.">
+    <meta name="twitter:image" content="https://webmunicipal.online/public/img/logos_sanvicente/logo_misanvicente.png">
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-TE38TW45ZY"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-TE38TW45ZY');
+    </script>
     <link rel="stylesheet" href="../../css/style-register.css">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
 </head>
